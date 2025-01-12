1c777764e202b8899bd4cdf84aa1f8afdb0c571c
叶翀睿 <yyccrr@xiechongruideMacBook-Pro.local>
2025-01-12 18:38:10 +0800

    support database init

--- backend/app.py
+++ backend/app.py
@@ -1,57 +1,29 @@
+from typing import Any, Dict, List
+
 from flask import Flask, Response, jsonify, request
 from flask_cors import CORS
+from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app)
 
 
 
-CARDS = [
-    {
-        "id": 1,
-        "title": "Who will win the president election?",
-        "options": [
-            {"label": "A", "percentage": "45%"},
-            {"label": "B", "percentage": "40%"},
-            {"label": "C", "percentage": "10%"},
-            {"label": "D", "percentage": "5%"},
-        ],
-    },
-    {
-        "id": 2,
-        "title": "Who will win the president election?",
-        "options": [
-            {"label": "A", "percentage": "45%"},
-            {"label": "B", "percentage": "40%"},
-            {"label": "C", "percentage": "10%"},
-            {"label": "D", "percentage": "5%"},
-        ],
-    },
-    {
-        "id": 3,
-        "title": "Who will win the president election?",
-        "options": [
-            {"label": "A", "percentage": "45%"},
-            {"label": "B", "percentage": "40%"},
-            {"label": "C", "percentage": "10%"},
-            {"label": "D", "percentage": "5%"},
-        ],
-    },
-        {
-        "id": 1,
-        "title": "Who will win the president election?",
-        "options": [
-            {"label": "A", "percentage": "45%"},
-            {"label": "B", "percentage": "40%"},
-            {"label": "C", "percentage": "10%"},
-            {"label": "D", "percentage": "5%"},
-        ],
-    },
-]
+client: MongoClient[Any] = MongoClient("mongodb://localhost:27017/")
+db = client['electionDB'] 
+collection = db['cards'] 
 
 @app.route('/api/cards', methods=['GET'])
 def get_cards() -> Response:
-    return jsonify(CARDS)
+    try:
+        cards: List[Dict[str, Any]] = list(collection.find({}, {"_id": 0}))
+        response = jsonify(cards)
+        response.status_code = 200
+        return response
+    except Exception as e:
+        error_response = jsonify({"error": str(e)})
+        error_response.status_code = 500
+        return error_response
 
 if __name__ == '__main__':
     app.run(debug=True)
