95db01fb1b25d9c9d84315382e7100027f883c5a
Haofei Yu <1125027232@qq.com>
2025-01-21 00:08:24 +0800

    feat(utils): update and reorg converter (#53)

--- swm/data.py
+++ swm/data.py
@@ -3,7 +3,7 @@ from typing import Dict, List, Optional, Tuple, Union
 from pydantic import BaseModel, Field
 
 
-class Opinion(BaseModel):
+class PolyMarketData(BaseModel):
     event_id: str
     market_id: str
     question: str
@@ -15,6 +15,7 @@ class Opinion(BaseModel):
     end_ts: Optional[float] = Field(default=None)
     tags: Optional[List[str]] = Field(default=None)
     tag_ids: Optional[List[str]] = Field(default=None)
+    category: Optional[List[str]] = Field(default=None)
     time_series: Optional[Dict[str, List[Dict[str, Union[int, float]]]]] = Field(
         default=None
     )
@@ -23,28 +24,11 @@ class Opinion(BaseModel):
     )
 
     @classmethod
-    def from_dict(cls, data: Dict) -> 'Opinion':
+    def from_dict(cls, data: Dict) -> 'PolyMarketData':
         if 'breakpoint_ts_pairs' in data and data['breakpoint_ts_pairs']:
             data['breakpoint_ts_pairs'] = {
                 key: [tuple(pair) for pair in pairs]
                 for key, pairs in data['breakpoint_ts_pairs'].items()
             }
 
-        return cls(**{k: v for k, v in data.items() if k in cls.__annotations__})
-
-
-class Event(BaseModel):
-    event_id: str
-    market_id: str
-    question: str
-    resolution_source: str
-    volumn: Optional[float] = Field(default=None)
-    end_ts: Optional[float] = Field(default=None)
-    outcome: Optional[str] = Field(default=None)
-    discrption: Optional[str] = Field(default=None)
-    tags: Optional[List[str]] = Field(default=None)
-    tag_ids: Optional[List[str]] = Field(default=None)
-
-    @classmethod
-    def from_dict(cls, data: Dict) -> 'Event':
         return cls(**{k: v for k, v in data.items() if k in cls.__annotations__})
