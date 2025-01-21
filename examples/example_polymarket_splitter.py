19d4eabea329b987e71ea893c7f07eaebc00e401
Haofei Yu <1125027232@qq.com>
2025-01-21 17:48:24 +0800

    feat(crawler): update newsapi usage and save format (#62)
    * update converter
    
    * update news api
    
    * update news api
    
    * update splitter
    
    * update splitter
    
    * update splitter
    
    * fix splitter bug
    
    * update news api format

--- examples/example_polymarket_splitter.py
+++ examples/example_polymarket_splitter.py
@@ -17,12 +17,12 @@ def parse_args():
         '--input_files',
         nargs='+',
         default=[
-            '../data/processed/polymarket_data_processed_Crypto.jsonl',
-            '../data/processed/polymarket_data_processed_Sports.jsonl',
-            '../data/processed/polymarket_data_processed_Election.jsonl',
-            '../data/processed/polymarket_data_processed_Other.jsonl',
-            '../data/processed/polymarket_data_processed_Politics.jsonl',
-            '../data/processed/polymarket_data_processed.jsonl',
+            '../data/processed_polymarket/polymarket_data_processed_Crypto.jsonl',
+            '../data/processed_polymarket/polymarket_data_processed_Sports.jsonl',
+            '../data/processed_polymarket/polymarket_data_processed_Election.jsonl',
+            '../data/processed_polymarket/polymarket_data_processed_Other.jsonl',
+            '../data/processed_polymarket/polymarket_data_processed_Politics.jsonl',
+            '../data/processed_polymarket/polymarket_data_processed.jsonl',
         ],
     )
     parser.add_argument(
