91ac864edf76a4a70a2754fd990d65b922f447ef
Haofei Yu <1125027232@qq.com>
2025-01-26 04:41:55 +0800

    fix(load): save and load does not work (#80)
    * update
    
    * update

--- scripts/train_basicswm.sh
+++ scripts/train_basicswm.sh
@@ -57,9 +57,10 @@ CUDA_VISIBLE_DEVICES=4 python train_basicswm.py \
 CUDA_VISIBLE_DEVICES=8 python train_basicswm.py \
 --train-data-path ../data/splitted_polymarket/polymarket_data_processed_Crypto_test.jsonl \
 --valid-data-path ../data/splitted_polymarket/polymarket_data_processed_Crypto_test.jsonl \
---output-dir ../saves/saves_crypto_basicswm \
---cache-dir ../cache/cache_crypto_basicswm \
+--output-dir ../saves/saves_crypto_basicswm_sanitycheck \
+--cache-dir ../cache/cache_crypto_basicswm_sanitycheck \
 --epochs 160 \
 --save-steps 10 \
 --eval-steps 10 \
---warmup-steps 0
+--warmup-steps 0 \
+--sanity-check
