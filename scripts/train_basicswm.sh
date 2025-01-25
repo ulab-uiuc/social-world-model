c338a2a61bbe59e1a67f750ecf18084659e44cd2
Haofei Yu <1125027232@qq.com>
2025-01-26 06:36:31 +0800

    fix(train): fix overfitting does not work error (#81)
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- scripts/train_basicswm.sh
+++ scripts/train_basicswm.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=9 python train_basicswm.py \
 --cache-dir ../cache/cache_crypto_basicswm \
 --train-batch-size 16 \
 --eval-batch-size 16 \
---epochs 3
+--epochs 10
 
 CUDA_VISIBLE_DEVICES=8 python train_basicswm.py \
 --train-data-path ../data/splitted_polymarket/polymarket_data_processed_Sports_train.jsonl \
@@ -14,7 +14,7 @@ CUDA_VISIBLE_DEVICES=8 python train_basicswm.py \
 --cache-dir ../cache/cache_sports_basicswm \
 --train-batch-size 16 \
 --eval-batch-size 16 \
---epochs 3
+--epochs 10
 
 CUDA_VISIBLE_DEVICES=7 python train_basicswm.py \
 --train-data-path ../data/splitted_polymarket/polymarket_data_processed_Other_train.jsonl \
@@ -23,7 +23,7 @@ CUDA_VISIBLE_DEVICES=7 python train_basicswm.py \
 --cache-dir ../cache/cache_other_basicswm \
 --train-batch-size 16 \
 --eval-batch-size 16 \
---epochs 3
+--epochs 10
 
 CUDA_VISIBLE_DEVICES=6 python train_basicswm.py \
 --train-data-path ../data/splitted_polymarket/polymarket_data_processed_Election_train.jsonl \
@@ -32,25 +32,25 @@ CUDA_VISIBLE_DEVICES=6 python train_basicswm.py \
 --cache-dir ../cache/cache_election_basicswm \
 --train-batch-size 16 \
 --eval-batch-size 16 \
---epochs 3
+--epochs 10
 
-CUDA_VISIBLE_DEVICES=5 python train_basicswm.py \
+CUDA_VISIBLE_DEVICES=4 python train_basicswm.py \
 --train-data-path ../data/splitted_polymarket/polymarket_data_processed_Politics_train.jsonl \
 --valid-data-path ../data/splitted_polymarket/polymarket_data_processed_Politics_dev.jsonl \
 --output-dir ../saves/saves_politics_basicswm \
 --cache-dir ../cache/cache_politics_basicswm \
 --train-batch-size 16 \
 --eval-batch-size 16 \
---epochs 3
+--epochs 10
 
-CUDA_VISIBLE_DEVICES=4 python train_basicswm.py \
+CUDA_VISIBLE_DEVICES=3 python train_basicswm.py \
 --train-data-path ../data/splitted_polymarket/polymarket_data_processed_train.jsonl \
 --valid-data-path ../data/splitted_polymarket/polymarket_data_processed_dev.jsonl \
 --output-dir ../saves/saves_all_basicswm \
 --cache-dir ../cache/cache_all_basicswm \
 --train-batch-size 16 \
 --eval-batch-size 16 \
---epochs 3
+--epochs 10
 
 
 # for sanity check
