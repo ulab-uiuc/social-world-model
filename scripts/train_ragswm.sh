dced1865fbd5b0483729b926ced380a695292278
lwaekfjlk <1125027232@qq.com>
2025-01-26 09:02:23 +0000

    update scripts

--- scripts/train_ragswm.sh
+++ scripts/train_ragswm.sh
@@ -8,24 +8,10 @@ CUDA_VISIBLE_DEVICES=0 python train_ragswm.py \
 --eval-batch-size 16 \
 --retriever-top-k 50 \
 --epochs 10
---corpus-data-path ../data/splitted_polymarket/polymarket_data_processed_train.jsonl \
---output-dir ../saves/saves_crypto_ragswm \
---cache-dir ../cache/cache_crypto_ragswm \
---train-batch-size 16 \
---eval-batch-size 16 \
---retriever-top-k 50 \
---epochs 10
 
 CUDA_VISIBLE_DEVICES=1 python train_ragswm.py \
 --train-data-path ../data/splitted_polymarket/polymarket_data_processed_Sports_train.jsonl \
 --valid-data-path ../data/splitted_polymarket/polymarket_data_processed_Sports_dev.jsonl \
---corpus-data-path ../data/splitted_polymarket/polymarket_data_processed_train.jsonl \
---output-dir ../saves/saves_sports_ragswm \
---cache-dir ../cache/cache_sports_ragswm \
---train-batch-size 16 \
---eval-batch-size 16 \
---retriever-top-k 50 \
---epochs 10
 --corpus-data-path ../data/splitted_polymarket/polymarket_data_processed_train.jsonl \
 --output-dir ../saves/saves_sports_ragswm \
 --cache-dir ../cache/cache_sports_ragswm \
@@ -44,24 +30,10 @@ CUDA_VISIBLE_DEVICES=7 python train_ragswm.py \
 --eval-batch-size 16 \
 --retriever-top-k 50 \
 --epochs 10
---corpus-data-path ../data/splitted_polymarket/polymarket_data_processed_train.jsonl \
---output-dir ../saves/saves_other_ragswm \
---cache-dir ../cache/cache_other_ragswm \
---train-batch-size 16 \
---eval-batch-size 16 \
---retriever-top-k 50 \
---epochs 10
 
 CUDA_VISIBLE_DEVICES=9 python train_ragswm.py \
 --train-data-path ../data/splitted_polymarket/polymarket_data_processed_Election_train.jsonl \
 --valid-data-path ../data/splitted_polymarket/polymarket_data_processed_Election_dev.jsonl \
---corpus-data-path ../data/splitted_polymarket/polymarket_data_processed_train.jsonl \
---output-dir ../saves/saves_election_ragswm \
---cache-dir ../cache/cache_election_ragswm \
---train-batch-size 16 \
---eval-batch-size 16 \
---retriever-top-k 50 \
---epochs 10
 --corpus-data-path ../data/splitted_polymarket/polymarket_data_processed_train.jsonl \
 --output-dir ../saves/saves_election_ragswm \
 --cache-dir ../cache/cache_election_ragswm \
@@ -80,39 +52,10 @@ CUDA_VISIBLE_DEVICES=5 python train_ragswm.py \
 --eval-batch-size 16 \
 --retriever-top-k 50 \
 --epochs 10
---corpus-data-path ../data/splitted_polymarket/polymarket_data_processed_train.jsonl \
---output-dir ../saves/saves_politics_ragswm \
---cache-dir ../cache/cache_politics_ragswm \
---train-batch-size 16 \
---eval-batch-size 16 \
---retriever-top-k 50 \
---epochs 10
 
 CUDA_VISIBLE_DEVICES=0 python train_ragswm.py \
 --train-data-path ../data/splitted_polymarket/polymarket_data_processed_train.jsonl \
 --valid-data-path ../data/splitted_polymarket/polymarket_data_processed_dev.jsonl \
---corpus-data-path ../data/splitted_polymarket/polymarket_data_processed_train.jsonl \
---output-dir ../saves/saves_all_ragswm \
---cache-dir ../cache/cache_all_ragswm \
---train-batch-size 16 \
---eval-batch-size 16 \
---retriever-top-k 50 \
---epochs 10
-
-
-# for sanity check
-CUDA_VISIBLE_DEVICES=2 python train_ragswm.py \
---train-data-path ../data/splitted_polymarket/polymarket_data_processed_Crypto_test.jsonl \
---valid-data-path ../data/splitted_polymarket/polymarket_data_processed_Crypto_test.jsonl \
---corpus-data-path ../data/splitted_polymarket/polymarket_data_processed_Crypto_train.jsonl \
---output-dir ../saves/saves_crypto_ragswm_sanitycheck \
---cache-dir ../cache/cache_crypto_ragswm_sanitycheck \
---retriever-top-k 50 \
---epochs 160 \
---save-steps 10 \
---eval-steps 10 \
---warmup-steps 0 \
---sanity-check
 --corpus-data-path ../data/splitted_polymarket/polymarket_data_processed_train.jsonl \
 --output-dir ../saves/saves_all_ragswm \
 --cache-dir ../cache/cache_all_ragswm \
