9f283d673686ec4e32a28eb52cea98b193a6fd61
Haofei Yu <1125027232@qq.com>
2025-01-29 04:55:18 +0800

    feat(dataset): support news saving (#98)
    * update
    
    * update

--- swm/dataset.py
+++ swm/dataset.py
@@ -441,6 +441,7 @@ class BasicPolyMarketDatasetWithEventForReasoner(BaseDataset):
                 'input_ids': [],
                 'attention_mask': [],
                 'p_scores': [],
+                'news': [],
                 'label': None,
                 'market_id': None,
                 'event_id': None,
@@ -496,6 +497,7 @@ class BasicPolyMarketDatasetWithEventForReasoner(BaseDataset):
                         encoding['attention_mask'].squeeze(0)
                     )
                     grouped_points[key]['p_scores'].append(event['score'])
+                    grouped_points[key]['news'].append(event['news'])
 
         return [self._process_group(group) for group in grouped_points.values()]
 
@@ -526,6 +528,7 @@ class BasicPolyMarketDatasetWithEventForReasoner(BaseDataset):
             'market_id': group['market_id'],
             'event_id': group['event_id'],
             't': group['t'],
+            'news': group['news'],
         }
 
     def _build_prompt(
