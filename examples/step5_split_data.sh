dd0d4f31ebd0e2e6df368d5315c4f6e2defe1155
Haofei Yu <1125027232@qq.com>
2026-01-23 05:53:11 +0800

    fix(attributor): fix attributor eval device error (#185)
    * update
    
    * update
    
    * support full timeseries data
    
    * change it into little weight
    
    * change it into little weight
    
    * update

--- examples/step5_split_data.sh
+++ examples/step5_split_data.sh
@@ -7,6 +7,18 @@ cd "$(dirname "$0")/.."
 DATA_DIR="/mnt/data_from_server1/haofeiy2/social-world-model/data"
 CUTOFF="2025-11-01"
 
+python examples/step5_split_data.py \
+    --input_file "${DATA_DIR}/processed_kalshi_v2_0102/kalshi_data_processed_with_news.jsonl" \
+    --output_dir "${DATA_DIR}/splitted_kalshi_v2_0102" \
+    --cutoff_date $CUTOFF
+
+# Polymarket
+python examples/step5_split_data.py \
+    --input_file "${DATA_DIR}/processed_polymarket_v2_0102/polymarket_data_processed_with_news.jsonl" \
+    --output_dir "${DATA_DIR}/splitted_polymarket_v2_0102" \
+    --cutoff_date $CUTOFF
+
+
 # Kalshi
 python examples/step5_split_data.py \
     --input_file "${DATA_DIR}/processed_kalshi_v2_0102/kalshi_data_processed_with_news_attributed.jsonl" \
